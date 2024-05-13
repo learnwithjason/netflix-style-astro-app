9c28d462f4153e002418ac852eabe0691d0a7239
Jason Lengstorf <jason@lengstorf.com>
2024-05-13 16:36:32 -0700

    wip: broken setup with Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-import db from "@astrojs/db";
+import db from '@astrojs/db';
+import react from '@astrojs/react';
+import clerk from 'astro-clerk-auth';
 
-import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), react()]
+  output: 'server',
+  integrations: [db(), react(), clerk({
+    afterSignInUrl: '/',
+    afterSignUpUrl: '/'
+  })],
+  adapter: netlify()
 });
